403d1e560a578c51b4d207becd11f164e0d0180b
ryo-murai <murai.ry@gmail.com>
2019-02-15 11:21:15 +0900

    created by spring initializr

---
+++ src/main/java/com/example/studyswagger/StudyswaggerApplication.java
@@ -0,0 +1,14 @@
+package com.example.studyswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyswaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyswaggerApplication.class, args);
+	}
+
+}
+
